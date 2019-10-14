d841a25314fd0f153faf082537b4aa2a31837209
Stefano Mercadante <stefano.mercadante@scaiconsulting.it>
2019-10-14 10:55:57 +0200

    git pages

---
+++ build/precache-manifest.190f4e1d756117ae0eba10631522ead4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68acab4be7bbbe062d9ee8aa6e783bab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "568895a9c00ae82790e1",
+    "url": "/robofriends/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "3f663b97b33f4983d06f",
+    "url": "/robofriends/static/css/main.cacee46c.chunk.css"
+  },
+  {
+    "revision": "568895a9c00ae82790e1",
+    "url": "/robofriends/static/js/2.4f89b62c.chunk.js"
+  },
+  {
+    "revision": "3f663b97b33f4983d06f",
+    "url": "/robofriends/static/js/main.bf84a0cc.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
